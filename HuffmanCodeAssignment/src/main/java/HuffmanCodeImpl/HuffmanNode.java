9fdc06758a92220f4309a7c62c0a6b208a3d5509
Chase Ingersol <cingersol@thebrain.com>
2022-10-22 16:15:51 -0700

    Project setup

---
+++ HuffmanCodeAssignment/src/main/java/HuffmanCodeImpl/HuffmanNode.java
@@ -0,0 +1,11 @@
+package HuffmanCodeImpl;
+
+// node class is the basic structure
+// of each node present in the Huffman - tree.
+class HuffmanNode {
+    char c;
+    int freq;
+
+    HuffmanNode left;
+    HuffmanNode right;
+}
---
+++ HuffmanCodeAssignment/src/main/java/HuffmanCodeImpl/Tester.java
@@ -0,0 +1,12 @@
+package HuffmanCodeImpl;
+
+import java.util.Scanner;
+
+public class Tester {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        
+
+    }
+}
