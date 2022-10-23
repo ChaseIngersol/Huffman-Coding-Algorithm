1151fd04a2ae66c10812ca76911c2a5961eb76ad
Chase Ingersol <cingersol@thebrain.com>
2022-10-22 19:20:41 -0700

    Start from scratch

--- HuffmanCodeAssignment/src/main/java/HuffmanCodeImpl/Node.java
+++ HuffmanCodeAssignment/src/main/java/HuffmanCodeImpl/Node.java
@@ -1,22 +1,17 @@
 package HuffmanCodeImpl;
 
 class Node {
-    //storing character in ch variable of type character
-    Character ch;
-    //storing frequency in freq variable of type int
-    Integer freq;
-    //initially both child (left and right) are null
+    char ch;
+    int freq;
     Node left = null;
     Node right = null;
 
-    //creating a constructor of the Node class
-    Node(Character ch, Integer freq) {
+    Node(char ch, int freq) {
         this.ch = ch;
         this.freq = freq;
     }
 
-    //creating a constructor of the Node class
-    public Node(Character ch, Integer freq, Node left, Node right) {
+    public Node(char ch, int freq, Node left, Node right) {
         this.ch = ch;
         this.freq = freq;
         this.left = left;
